c4660312d1a119d2dbc0bea5bd2b92ca12e47f9e
BlisS <contacto@fixter.org>
2017-04-21 14:24:34 -0500

    static2

--- social_api/settings.py
+++ social_api/settings.py
@@ -135,9 +135,9 @@ USE_TZ = True
 
 STATIC_URL = '/static/'
 
-STATICFILES_DIRS = (os.path.join(BASE_DIR,"static"),)
+STATICFILES_DIRS = (os.path.join(BASE_DIR,"staticfiles"),)
 # STATIC_ROOT = 'staticfiles'
-STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 
 REST_FRAMEWORK = {
     'DEFAULT_AUTHENTICATION_CLASSES': (
