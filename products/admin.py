3704efff19019a52f336dee2935e85b5d53995c4
BlisS <contacto@fixter.org>
2017-04-21 18:22:04 -0500

    prepopulated

--- products/admin.py
+++ products/admin.py
@@ -2,5 +2,12 @@ from django.contrib import admin
 from .models import Product, Category, Document
 
 admin.site.register(Product)
-admin.site.register(Category)
+
 admin.site.register(Document)
+
+
+class CategoryAdmin(admin.ModelAdmin):
+	list_display = ['id','name']
+	prepopulated_fields={'slug':('name',)}
+
+admin.site.register(Category, CategoryAdmin)
