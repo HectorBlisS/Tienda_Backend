a68c93a05ec9142de793fa2b43dc9907c23ca33b
BlisS <contacto@fixter.org>
2017-04-21 19:10:36 -0500

    media

--- social_api/settings.py
+++ social_api/settings.py
@@ -155,8 +155,8 @@ STATICFILES_DIRS = (os.path.join(BASE_DIR,"staticfiles"),)
 # STATIC_ROOT = 'staticfiles'
 STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 
-#MEDIA_URL = '/media/'
-#MEDIA_ROOT = os.path.join(BASE_DIR,'media/')
+MEDIA_URL = '/media/'
+MEDIA_ROOT = os.path.join(BASE_DIR,'media/')
 
 REST_FRAMEWORK = {
     'DEFAULT_AUTHENTICATION_CLASSES': (
