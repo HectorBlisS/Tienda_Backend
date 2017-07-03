41bc73a5c52cc0a9f88f4a66eb90dccc0891202d
Topi99 <nokoayzack@gmail.com>
2017-07-02 23:26:49 -0500

    permissions_classes added to the cloud_storage views.py

--- cloud_storage/views.py
+++ cloud_storage/views.py
@@ -1,11 +1,8 @@
-from social_api import settings
-from django.views.generic import View
 from django.shortcuts import render, HttpResponse
+from social_api.settings import BASE_DIR 
+from django.views.generic import View
 from rest_framework import permissions
 from rest_framework.views import APIView
-
-
-# Create your views here.
 
 """ Usando gsutil """
 
@@ -15,10 +12,8 @@ class GetSignedUrl(APIView):
 
 	def get(self, request):
 
-		if product.users.filter(id=request.user.id).exists():
-			result = subprocess.run(["gsutil", "signurl", "-d", "10m", settings.BASE_DIR+"/tienda-eric-e3120f4dca2e.json", "gs://tienda-eric/BRIEF.pdf"], stdout=subprocess.PIPE)
-			result = result.stdout.decode("utf-8").split(" ")
-			return HttpResponse(result[3][9:])
-
-		return HttpResponse("Producto no adquirido")
+		result = subprocess.run(["gsutil", "signurl", "-d", "10m", BASE_DIR+"/tienda-eric-e3120f4dca2e.json", "gs://tienda-eric/BRIEF.pdf"], stdout=subprocess.PIPE)
+		result = result.stdout.decode("utf-8").split(" ")
+		# print(request.authenticators[1].__dict__)
+		return HttpResponse(result[3][9:])
 
